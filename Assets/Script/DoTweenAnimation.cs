69d5cc296af8d70a5bb22c45e83fc305d83680af
Saadiii <saad22oct2006@gmail.com>
2026-06-11 21:45:20 +0500

    Easing_Methods

--- Assets/Script/DoTweenAnimation.cs
+++ Assets/Script/DoTweenAnimation.cs
@@ -5,10 +5,7 @@ public class DoTweenAnimation : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        transform.DOMoveX(5f, 2f).OnComplete(() =>
-        {
-            transform.DOMoveY(50f, 1f);
-        });
+        transform.DOMoveX(10f, 2f).SetEase(Ease.OutQuad);
     }
 
     // Update is called once per frame
