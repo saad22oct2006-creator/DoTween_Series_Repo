629f7457eb4961447b16c0f2bf2d45b01fc1fbb6
Saadiii <saad22oct2006@gmail.com>
2026-06-13 22:28:36 +0500

    DelayinDoTween

--- Assets/Script/DoTweenAnimation.cs
+++ Assets/Script/DoTweenAnimation.cs
@@ -2,10 +2,17 @@ using UnityEngine;
 using DG.Tweening;
 public class DoTweenAnimation : MonoBehaviour
 {
+    public Transform cube1;
+    public Transform cube2;
+    public Transform cube3;
+    
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        transform.DOMoveX(10f, 2f).SetEase(Ease.OutQuad);
+        cube1.DOMoveX(10f, 2f).SetDelay(0f).SetEase(Ease.OutQuad);
+        cube2.DOMoveX(10f, 2f).SetDelay(0.2f).SetEase(Ease.OutQuad);
+        cube3.DOMoveX(10f, 2f).SetDelay(0.4f).SetEase(Ease.OutQuad);
     }
 
     // Update is called once per frame
