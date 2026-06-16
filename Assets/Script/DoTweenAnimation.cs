135a499627a845023c54ac90cae8562d8d900078
Saadiii <saad22oct2006@gmail.com>
2026-06-16 23:03:24 +0500

    DoScaleUpdate

--- Assets/Script/DoTweenAnimation.cs
+++ Assets/Script/DoTweenAnimation.cs
@@ -10,9 +10,13 @@ public class DoTweenAnimation : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        cube1.DOMoveX(10f, 2f).SetDelay(0f).SetEase(Ease.OutQuad);
-        cube2.DOMoveX(10f, 2f).SetDelay(0.2f).SetEase(Ease.OutQuad);
-        cube3.DOMoveX(10f, 2f).SetDelay(0.4f).SetEase(Ease.OutQuad);
+        //cube1.DOMoveX(10f, 2f).SetDelay(0f).SetEase(Ease.OutQuad);
+        //cube2.DOMoveX(10f, 2f).SetDelay(0.2f).SetEase(Ease.OutQuad);
+        //cube3.DOMoveX(10f, 2f).SetDelay(0.4f).SetEase(Ease.OutQuad);
+
+        cube1.DOScale(new Vector3(5f, 5f, 5f), 0.8f).SetDelay(0.3f).SetEase(Ease.InElastic);
+
+
     }
 
     // Update is called once per frame
