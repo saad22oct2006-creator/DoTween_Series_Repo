05cecbf0bf4db4b3c0e14f13abfb934eb413ef72
Saadiii <saad22oct2006@gmail.com>
2026-06-27 16:43:04 +0500

    SetLoop'sInDoTween

--- Assets/Script/DoTweenAnimation.cs
+++ Assets/Script/DoTweenAnimation.cs
@@ -13,8 +13,7 @@ public class DoTweenAnimation : MonoBehaviour
     void Start()
     {
 
-        Sequence cube1Seq = DOTween.Sequence();
-        Sequence cube2Seq = DOTween.Sequence();
+        //Sequence cube1Seq = DOTween.Sequence();
 
         //cube1.DOMoveX(10f, 2f).SetDelay(0f).SetEase(Ease.OutQuad);
         //cube2.DOMoveX(10f, 2f).SetDelay(0.2f).SetEase(Ease.OutQuad);
@@ -22,16 +21,16 @@ public class DoTweenAnimation : MonoBehaviour
 
         //cube1.DOScale(new Vector3(5f, 5f, 5f), 0.8f).SetDelay(0.3f).SetEase(Ease.InElastic);
 
+
+        /*cube1Seq.Append(cube1.DOMoveZ(10, 0.3f).SetEase(Ease.Flash))
+            .Join(cube1.DORotate(new Vector3(0, 360, 0), 0.3f).SetEase(Ease.Flash))
+            .Join(cube1.GetComponent<Renderer>().material.DOColor(Color.magenta, 1f).SetEase(Ease.Flash));*/
         
-        cube1Seq.Append(cube1.DOMoveZ(10,0.3f).SetEase(Ease.Flash)).SetDelay(1f)
-            .Append(cube1.DOScale(new Vector3(5f,5f,5f),0.3f).SetEase(Ease.InBounce));
-
-        cube2Seq.PrependInterval(2f);
-        cube2Seq.Append(cube2.DORotate(new Vector3(0f,180f,0f),0.3f).SetEase(Ease.InFlash)).SetDelay(1f)
-            .Append(cube2.DOMoveZ(20f,0.3f).SetEase(Ease.InBounce));
-
-
-
+        cube1.DOMoveY(20f, 1f).SetLoops(-1, LoopType.Restart).SetEase(Ease.Linear);
+        cube2.DORotate(new Vector3(0f,360f,0f),1f, RotateMode.FastBeyond360).SetLoops(-1, LoopType.Restart).SetEase(Ease.Linear);
+        cube3.DOScale(new Vector3(0.5f,0.5f,0.5f),1f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.Linear);
+        
+        
     }
 
     // Update is called once per frame
