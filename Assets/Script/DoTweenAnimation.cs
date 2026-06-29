76f4dbde2a5d21dee82fb4e5b585b030c133d369
Saadiii <saad22oct2006@gmail.com>
2026-06-29 23:40:01 +0500

    PlayPauseKilled_in_DoTween

--- Assets/Script/DoTweenAnimation.cs
+++ Assets/Script/DoTweenAnimation.cs
@@ -7,12 +7,21 @@ public class DoTweenAnimation : MonoBehaviour
     public Transform cube3;
     
     
-    
+    Tween myTween;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
 
+        myTween = cube1.DOMoveX(10f, 2f).SetDelay(0f).SetLoops(-1,LoopType.Yoyo)
+            .SetEase(Ease.OutQuad);
+        
+        
+        
+        
+        
+        
+        
         //Sequence cube1Seq = DOTween.Sequence();
 
         //cube1.DOMoveX(10f, 2f).SetDelay(0f).SetEase(Ease.OutQuad);
@@ -26,9 +35,11 @@ public class DoTweenAnimation : MonoBehaviour
             .Join(cube1.DORotate(new Vector3(0, 360, 0), 0.3f).SetEase(Ease.Flash))
             .Join(cube1.GetComponent<Renderer>().material.DOColor(Color.magenta, 1f).SetEase(Ease.Flash));*/
         
-        cube1.DOMoveY(20f, 1f).SetLoops(-1, LoopType.Restart).SetEase(Ease.Linear);
+        /*cube1.DOMoveY(20f, 1f).SetLoops(-1, LoopType.Restart).SetEase(Ease.Linear);
         cube2.DORotate(new Vector3(0f,360f,0f),1f, RotateMode.FastBeyond360).SetLoops(-1, LoopType.Restart).SetEase(Ease.Linear);
-        cube3.DOScale(new Vector3(0.5f,0.5f,0.5f),1f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.Linear);
+        cube3.DOScale(new Vector3(0.5f,0.5f,0.5f),1f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.Linear);*/
+        
+        
         
         
     }
@@ -36,6 +47,22 @@ public class DoTweenAnimation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myTween.Pause();
+            Debug.Log("Pause");
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            myTween.Play();
+            Debug.Log("Playing");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            myTween.Kill();
+            Debug.Log("Anim Killed");
+        }
     }
 }
